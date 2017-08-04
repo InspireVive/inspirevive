8a7bc909e817a3414858a3c4e649027600e25715
Jared King <j@jaredtking.com>
2017-08-04 08:06:36 -0500

    remove old social media tests

--- app/users/tests/UserTest.php
+++ app/users/tests/UserTest.php
@@ -22,12 +22,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 
     public static function tearDownAfterClass()
     {
-        foreach ([self::$user, self::$twitterProfile, self::$instagramProfile] as $m) {
-            if ($m) {
-                $m->grantAllPermissions();
-                $m->delete();
-            }
-        }
+        self::$user->grantAllPermissions()->delete();
     }
 
     public function testRegisterUser()
@@ -84,100 +79,6 @@ class UserTest extends PHPUnit_Framework_TestCase
         $user->user_email = 'j@jaredtking.com';
 
         $this->assertEquals('https://secure.gravatar.com/avatar/ceed11e6dbd893ecbf3868d2018f0062?s=80&d=mm', $user->profilePicture(80));
-    }
-
-    public function testProfilePicturePreference()
-    {
-        $user = new User();
-        $user->facebook_id = 100;
-        $user->instagram_id = 100;
-        $user->profile_picture_preference = 'instagram';
-
-        $this->assertEquals('instagram_profile_picture', $user->profilePicture());
-    }
-
-    public function testFacebookConnected()
-    {
-        $user = new User();
-        $this->assertFalse($user->facebookConnected());
-
-        $user->facebook_id = 100;
-        $this->assertTrue($user->facebookConnected());
-    }
-
-    public function testFacebookProfile()
-    {
-        $user = new User();
-        $user->facebook_id = 100;
-
-        $profile = $user->facebookProfile();
-        $this->assertInstanceOf('app\facebook\models\FacebookProfile', $profile);
-        $this->assertEquals(100, $profile->id());
-    }
-
-    public function testProfilePictureFacebook()
-    {
-        $user = new User();
-        $user->facebook_id = 100;
-        $user->profile_picture_preference = 'facebook';
-
-        $this->assertEquals('https://graph.facebook.com/100/picture?width=80&height=80', $user->profilePicture(80));
-    }
-
-    public function testTwitterConnected()
-    {
-        $user = new User();
-        $this->assertFalse($user->twitterConnected());
-
-        $user->twitter_id = 100;
-        $this->assertTrue($user->twitterConnected());
-    }
-
-    public function testTwitterProfile()
-    {
-        $user = new User();
-        $user->twitter_id = 100;
-
-        $profile = $user->twitterProfile();
-        $this->assertInstanceOf('app\twitter\models\TwitterProfile', $profile);
-        $this->assertEquals(100, $profile->id());
-    }
-
-    public function testProfilePictureTwitter()
-    {
-        $user = new User();
-        $user->twitter_id = 100;
-        $user->profile_picture_preference = 'twitter';
-
-        $this->assertEquals('twitter_profile_picture', $user->profilePicture(80));
-    }
-
-    public function testInstagramConnected()
-    {
-        $user = new User();
-        $this->assertFalse($user->instagramConnected());
-
-        $user->instagram_id = 100;
-        $this->assertTrue($user->instagramConnected());
-    }
-
-    public function testInstagramProfile()
-    {
-        $user = new User();
-        $user->instagram_id = 100;
-
-        $profile = $user->instagramProfile();
-        $this->assertInstanceOf('app\instagram\models\InstagramProfile', $profile);
-        $this->assertEquals(100, $profile->id());
-    }
-
-    public function testProfilePictureInstagram()
-    {
-        $user = new User();
-        $user->instagram_id = 100;
-        $user->profile_picture_preference = 'instagram';
-
-        $this->assertEquals('instagram_profile_picture', $user->profilePicture(80));
     }
 
     /**
